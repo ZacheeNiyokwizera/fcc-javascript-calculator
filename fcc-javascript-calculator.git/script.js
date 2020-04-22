94c9aa754c6c296f445ffdf86c5e9a1a1d516802
Mr <zack@labs.ws>
2020-04-22 17:55:29 +0200

    added some cool buttons

--- fcc-javascript-calculator.git/script.js
+++ fcc-javascript-calculator.git/script.js
@@ -1,18 +1,50 @@
 // Determines button clicked via id
 function myFunction(id) {
-  document.calc.result.value+=id;
+  document.calc.result.value += id;
 }
 
 // Clears calculator input screen
 function clearScreen() {
-  document.calc.result.value="0";
+  document.calc.result.value = "0";
 }
 // Calculates input values
 function calculate() {
   try {
     var input = eval(document.calc.result.value);
-    document.calc.result.value=input;
-  } catch(err){
-      document.calc.result.value="Error";
-    }
+    document.calc.result.value = input;
+  } catch (err) {
+    document.calc.result.value = "Error";
+  }
 }
+
+// SWITCH the calculator 
+// $(document).ready(function () {
+//   $("#on").click({
+//     myScreen() {
+//       $("input").css("background-color", "black");
+
+//     }
+//   });
+// });
+
+$(document).ready(function () {
+  $("#on").on({
+
+
+    click: function () {
+      $("input").css("background-color", "grey");
+      $(".screen").val("0");
+    }
+  });
+});
+
+$(document).ready(function () {
+  $("#off").on({
+
+
+    click: function () {
+      $(".screen").css("background-color", "black");
+    }
+  });
+
+});
